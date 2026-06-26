c3ade620cd0ccfbad5856bc4245c6cb51ac287f5
Alexander Ulyanov <alexander.ulyanov@tibbo.com>
2026-06-26 11:29:52 +0300

    setup pwa, connect wasm

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [VitePWA({
+    registerType: "autoUpdate",
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      name: 'My Awesome App',
+      short_name: 'MyApp',
+      description: 'My Awesome App description',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
+    }
+  }), react()],
+});
